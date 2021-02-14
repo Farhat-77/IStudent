e1f281b2dad33ec1ecfff3dc2b2ff06261667e08
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-02-14 13:02:56 +0600

    Добавьте файлы проекта.

---
+++ IStudent/IStudent.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IStudent
+{
+    public interface IStudent
+    {
+        string Name { get; set; }
+        string FullName
+        {
+            get
+            {
+                return "FullName";
+            }
+            set
+            {
+
+            }
+        }
+        int Grades
+        {
+            get
+            {
+                return Grades[Student];
+            }
+            set
+            {
+                Grades[Student] = value;
+            }
+        }
+        string GetName
+        {
+            get
+            {
+                return "GetName";
+            }
+            set
+            {
+
+            }
+        }
+        string GetFullName
+        {
+            get
+            {
+                return "GetFullName";
+            }
+            set
+            {
+
+            }
+        }
+        public class Student : IStudent
+        {
+            public static int numberOfStudent;
+
+            private string Name;
+            public string FullName
+            {
+                get => Name;
+                set => Name = value;
+            }
+
+            private int Grades;
+            public int Grades
+            {
+                get => Grades;
+            }
+            public Student() => Grades = ++numberOfStudent;
+        }
+    }
+}
---
+++ IStudent/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IStudent
+{
+    public class Student
+    {
+        private String Name;
+        private int Grades;
+        private double GetAvgGrade;
+
+        public Student(String N, int G, double GAG)
+        {
+            Name = N;
+            Grades = G;
+            GetAvgGrade = GAG;
+        }
+
+        public String getName() { return Name; }
+        public int getGrades() { return Grades; }
+        public double geGetAvgGrade() { return GetAvgGrade; }
+
+        public String toString()
+        {
+            String temp = Name + "  " + Grades + "  " + GetAvgGrade;
+           return temp;
+        }
+    }
+}
